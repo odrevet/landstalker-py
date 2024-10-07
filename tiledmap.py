1a4a5af9ab9d0ec6eba4a9283d91f4f832527023
Olivier Drevet <odrevet@gmail.com>
2024-10-08 00:23:36 +0200

    move heightmap related code to heightmap class

--- tiledmap.py
+++ tiledmap.py
@@ -1,6 +1,4 @@
 import pygame
-import os
-import csv
 
 from pytmx.util_pygame import load_pygame
 
@@ -8,31 +6,6 @@ from hero import Hero
 from utils import cartesian_to_iso, iso_to_cartesian
 from debug import draw_heightmap
 
-class HeightmapCell:
-    def __init__(self, height, walkable):
-        self.height = height
-        self.walkable = walkable
-
-class Heightmap:
-    def __init__(self):
-        self.left_offset = 0
-        self.top_offset = 0
-        self.cells = []
-
-    def load(self, map_number):
-        map_filename = f"data/Map{map_number}_heightmap.csv"
-
-        if not os.path.exists(map_filename):
-            return
-
-        with open(map_filename, mode="r") as file:
-            csv_reader = csv.reader(file)
-            
-            header = next(csv_reader)
-            self.left_offset, self.top_offset = int(header[0], 16), int(header[1], 16)
-
-            for row in csv_reader:
-                self.cells.append([HeightmapCell(walkable=int(value[0], 16),height=int(value[1], 16)) for value in row])
 
 def load_map(map_number):
     map_filename = f"data/Map{map_number:03d}.tmx"
